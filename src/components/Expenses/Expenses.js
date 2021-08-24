5bf4317b76ec31ac0c82ccc0c224439897d5f819
Václav <strba.vaclav@gmail.com>
2021-08-24 12:01:05 +0200

    initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+  return (
+    <Card className='expenses'>
+      {props.expenses.map((ex) => (
+        <ExpenseItem
+          key={ex.id}
+          title={ex.title}
+          amount={ex.amount}
+          date={ex.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
